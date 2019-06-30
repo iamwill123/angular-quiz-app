c14f0591c727601910e8a19b120168ca1828eace
William Pei Yuan <williampeiyuan@gmail.com>
2019-06-30 18:28:12 -0400

    Fix - declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, QuestionFormComponent],
   imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-// Services are separate entities that isolate operations on data (fetching, saving, and so on). Thanks to Angular’s dependency injection (a way of giving each component what it asks for), these services are available to supply data to any component across the entire app.
 @Injectable({
   providedIn: 'root'
 })
